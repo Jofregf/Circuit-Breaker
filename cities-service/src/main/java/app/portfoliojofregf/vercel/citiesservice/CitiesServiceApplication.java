235fe4c9cee2a4892b6e4414fc553c131fb57806
Guillermo Jofre <jofregf@gmail.com>
2023-11-17 22:40:51 -0300

    feat: Hotel Service
    Creación del microservicio de Hotel

---
+++ cities-service/src/main/java/app/portfoliojofregf/vercel/citiesservice/CitiesServiceApplication.java
@@ -0,0 +1,13 @@
+package app.portfoliojofregf.vercel.citiesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesServiceApplication.class, args);
+	}
+
+}
---
+++ hotels-service/src/test/java/app/portfoliojofregf/vercel/hotelsservice/HotelsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.portfoliojofregf.vercel.hotelsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
